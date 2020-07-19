cfbc9b11d36c0dc287ffe55935965ab9d00550ab
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-07-19 15:41:16 +0200

    finished secret routes

--- routes/secret.js
+++ routes/secret.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const secretCtrl = require('../controllers/secret');
+
+router.get('/', secretCtrl.getAllSecrets);
+
+router.post('/', secretCtrl.createSecret);
+
+router.get(':id', secretCtrl.getOneSecret);
+
+router.put(':id', secretCtrl.updateSecret);
+
+router.delete(':id', secretCtrl.deleteSecret);
+
 module.exports = router;
