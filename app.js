a06088b9b50ba00c97fd9f7fdd6a4478a0b883c9
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-07-19 15:23:18 +0200

    set up cors middleware

--- app.js
+++ app.js
@@ -22,4 +22,17 @@ mongoose
 // MiddleWares
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
 module.exports = app;
