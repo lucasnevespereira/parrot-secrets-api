ae1b62cf5a9585e6df296f9b57e02216ab30abb6
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-07-19 15:10:28 +0200

    added mongoose connection w/env file

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
+
+// Setup Mongoose
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch((err) => {
+    console.log('Something went wrong connecting to database.', err);
+  });
 
 module.exports = app;
